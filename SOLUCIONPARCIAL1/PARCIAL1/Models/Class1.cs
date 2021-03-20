4f6bcb4026ce1584c51db293b2093a8cbe12af2a
Diego Xavier Tapia Inchauste <a2018118218@estudiantes.upsa.edu.bo>
2021-03-20 10:45:47 -0400

    2do Commit post creacion modelo

---
+++ SOLUCIONPARCIAL1/PARCIAL1/Models/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PARCIAL1.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "INGRESE ENTRE 3 A 30 CARACTERES")]
+        public string Description { get; set; }
+        [Required]
+        public DataType Price { get; set; }
+        public DataType LastBuy { get; set; }
+
+    }
+}
