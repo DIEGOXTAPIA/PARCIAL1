e85d67abf6fd7cb0ed06aec5a2699107247568fc
Diego Xavier Tapia Inchauste <a2018118218@estudiantes.upsa.edu.bo>
2021-03-20 09:53:42 -0400

    1ER Commit tras creacion de Sol c#

---
+++ SOLUCIONPARCIAL1/PARCIAL1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PARCIAL1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SOLUCIONPARCIAL1/PARCIAL1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PARCIAL1.WebApiApplication" Language="C#" %>
